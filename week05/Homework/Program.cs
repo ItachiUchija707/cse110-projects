f0c88020bb9345546e5d595b0bb5c12887b35f0c
ItachiUchija707 <abendeck@byupathway.edu>
2026-04-03 10:11:59 -0600

    week 5 learning activity completion

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
-    }
+        Assignment task1 = new Assignment("Roberto Rodriguez", "Fractions");
+        Console.WriteLine(task1.GetSummary());
+        MathAssignment task2 = new MathAssignment("Samuel Bennett", "Multiplication", "Section 7.3", "Problems 8-19");
+        Console.WriteLine($"{task2.GetSummary()}\n{task2.GetHomeworkList()}");
+        WritingAssignment task3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine($"{task3.GetSummary()}\n{task3.GetWritingInformation()}");
+    }   
 }
