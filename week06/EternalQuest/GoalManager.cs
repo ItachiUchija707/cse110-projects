49911b1713b8617d5b7146fcb374acf1d78c6ff6
ItachiUchija707 <abendeck@byupathway.edu>
2026-04-16 13:42:26 -0600

    feedback changes

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -1,7 +1,5 @@
 using System.Runtime.CompilerServices;
-// For creativity I added a level up system so the user gets more excited about achieving goals and increase the gamification of the program
-// the implentation of this level up system required the addition of methods and variables and some slight menu modifications. as well as 
-// changing the behavior for saving and loading the files. 
+
 public class GoalManager
 {
     private List<Goal> _goals = new List<Goal>();
--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,5 +1,7 @@
 using System;
-
+// For creativity I added a level up system so the user gets more excited about achieving goals and increase the gamification of the program
+// the implentation of this level up system required the addition of methods and variables and some slight menu modifications. as well as 
+// changing the behavior for saving and loading the files. 
 class Program
 {
     static void Main(string[] args)
